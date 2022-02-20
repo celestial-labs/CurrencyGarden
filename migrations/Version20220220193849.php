7f979e2c0149253af904f32ae67f357939137b11
Alexander <alexander.sokoll@digital-masters.de>
2022-02-20 20:39:30 +0100

    update migrations for MYSQL

--- migrations/Version20220220193849.php
+++ migrations/Version20220220193849.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220220191825 extends AbstractMigration
+final class Version20220220193849 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,7 @@ final class Version20220220191825 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE exchange_rate (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, base VARCHAR(255) NOT NULL, target VARCHAR(255) NOT NULL, rate VARCHAR(255) NOT NULL, updated_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
-        , created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
-        )');
+        $this->addSql('CREATE TABLE exchange_rate (id INT AUTO_INCREMENT NOT NULL, base VARCHAR(255) NOT NULL, target VARCHAR(255) NOT NULL, rate VARCHAR(255) NOT NULL, updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
