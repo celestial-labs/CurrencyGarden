355abdc32f56323b6d21fdc8eedffc4bab7cba32
Alexander <alexander.sokoll@digital-masters.de>
2022-02-20 20:25:49 +0100

    add migrations

--- migrations/Version20220220191825.php
+++ migrations/Version20220220191825.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220213143559 extends AbstractMigration
+final class Version20220220191825 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,9 @@ final class Version20220213143559 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE exchange_rate (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, base VARCHAR(255) NOT NULL, target VARCHAR(255) NOT NULL, rate VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE exchange_rate (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, base VARCHAR(255) NOT NULL, target VARCHAR(255) NOT NULL, rate VARCHAR(255) NOT NULL, updated_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        )');
     }
 
     public function down(Schema $schema): void
